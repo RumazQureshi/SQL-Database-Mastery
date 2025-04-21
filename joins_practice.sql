3be5b4b98bd9e262e75cb58d5e5ea2e22493ed74
Rumaz Naveed Qureshi <rumazqureshi@gmail.com>
2025-04-22 01:09:51 +0500

    Update joins_practice.sql

--- joins_practice.sql
+++ joins_practice.sql
@@ -78,27 +78,27 @@ where o.orderid is null;
 -- 10.Select Top 3 employees of company. Employees are ranked on the basis of no. of orders they have processed.
 -- (ROWNUM <= 3, ORDER BY number of orders processed)
 -- Query: 
--- select e.firstname || ' ' || e.lastname as employeename, count(o.orderid) as total_orders
--- from employees e
--- join orders o on e.employeeid = o.employeeid
--- group by e.firstname, e.lastname
--- order by total_orders desc
--- fetch first 3 rows only;
+select e.firstname || ' ' || e.lastname as employeename, count(o.orderid) as total_orders
+from employees e
+join orders o on e.employeeid = o.employeeid
+group by e.firstname, e.lastname
+order by total_orders desc
+fetch first 3 rows only;
 
 -- 11. Select orders in which products of neither ‘Meat/Poultry’ nor ‘Dairy Products’ categories exist. (Order ID)
 -- Query: 
--- select distinct o.orderid 
--- from orders o
--- join orderdetails od on o.orderid = od.orderid
--- join products p on p.productid = od.productid
--- join categories c on c.categoryid = p.categoryid
--- where o.orderid not in (
---     select o2.orderid 
---     from orders o2
---     join orderdetails od2 on o2.orderid = od2.orderid
---     join products p2 on p2.productid = od2.productid
---     join categories c2 on c2.categoryid = p2.categoryid
---     where c2.categoryname in ('meat/poultry', 'dairy products') );
+select distinct o.orderid 
+from orders o
+join orderdetails od on o.orderid = od.orderid
+join products p on p.productid = od.productid
+join categories c on c.categoryid = p.categoryid
+where o.orderid not in (
+    select o2.orderid 
+    from orders o2
+    join orderdetails od2 on o2.orderid = od2.orderid
+    join products p2 on p2.productid = od2.productid
+    join categories c2 on c2.categoryid = p2.categoryid
+    where c2.categoryname in ('meat/poultry', 'dairy products') );
 
 -- 12.Select total amount of each order.
 -- Result: Order ID, Total Amount
